dd9265c4082043c61837b475734eb9afe782957b
Ropitas028 <juansebastianrodasvelez028@gmail.com>
2024-12-10 23:45:26 -0600

    Add files via upload

---
+++ 1-intro-react/1-tienda-de-naranjas/src/App.jsx
@@ -0,0 +1,18 @@
+
+ import Section from "./components/Section"
+ import Article from "./components/Article"
+ import Footer from "./components/Footer"
+import Header from "./components/header"
+function App() {
+  return(
+    <div>
+    <Header />
+    <Section />
+    <Article />
+    <Footer />
+    </div>
+  )
+  
+}
+
+export default App
